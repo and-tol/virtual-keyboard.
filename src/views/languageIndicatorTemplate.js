f8d69591d689e3fac13db16ad7bdc162880bbcd5
andreitol <tolstoi.a@gmail.com>
2020-05-12 07:43:34 +0300

    init:languageIndicatorTemplate

---
+++ src/views/languageIndicatorTemplate.js
@@ -0,0 +1,8 @@
+/**
+ * Layout of the language Indicator
+ */
+const languageIndicatorTemplate = `<div class="language-indicator">
+    <p>Language indicator: <span id="language-text"></span></p>
+  </div>`;
+
+export default languageIndicatorTemplate;
